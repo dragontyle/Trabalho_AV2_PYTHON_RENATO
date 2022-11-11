63cd35b1f4adb56f813dea63298bbed7da0b20ad
Renato Boscarino <49559182+dragontyle@users.noreply.github.com>
2022-11-11 19:01:44 -0300

    Update Questão_3.py

--- Questão_3.py
+++ Questão_3.py
@@ -12,13 +12,13 @@ def horario(h, m):
 def saida():
     from time import sleep
     while True:
-        hora = int(input("digite a hora: "))
+        hora = int(input("Digite a hora: "))
         if hora < 0:
             break
-        minutos = int(input("digite os minutos: "))
-        print('-'*22)
-        print("CONVERTENDO O HORÁRIO")
-        print('-' * 22)
+        minutos = int(input("Digite os minutos: "))
+        print('-'* 34)
+        print("REALIZANDO A CONVERSÃO DO HORÁRIO")
+        print('-' * 34)
         sleep(1.5)
         horario(hora, minutos)
 
